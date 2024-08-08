8e36433a46a92964b1b0342e01873e4ae2d22da8
Sheikhwaseem28 <sheikhwaseem700800@gmail.com>
2024-08-08 14:35:17 +0530

    Updates

--- static/js/main.94b844ad.js
+++
@@ -1,2 +0,0 @@
-(()=>{"use strict";var o={p:"/Portfolio/"};o.p,o.p})();
-//# sourceMappingURL=main.94b844ad.js.map
---
+++ static/js/main.ed7e334a.js
@@ -0,0 +1,2 @@
+(()=>{"use strict";var p={p:"/portfolio/"};p.p,p.p})();
+//# sourceMappingURL=main.ed7e334a.js.map
